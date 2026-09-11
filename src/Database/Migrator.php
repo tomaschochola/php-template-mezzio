1f4362beb459eed730ecc111cc6a2e1351202a3e
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-09-11 17:02:01 +0200

    chore: consolidate pending workspace changes

--- src/Database/Migrator.php
+++ src/Database/Migrator.php
@@ -104,10 +104,10 @@ final readonly class Migrator
     {
         $this->execute(<<<'SQL'
             CREATE TABLE IF NOT EXISTS `migrations` (
-              `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              `selector` VARCHAR(255) NOT NULL UNIQUE,
-              `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-              `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+                `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                `selector` VARCHAR(255) NOT NULL UNIQUE,
+                `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
             SQL);
     }
--- src/Migration/CreateUsersTableMigration.php
+++ src/Migration/CreateUsersTableMigration.php
@@ -46,9 +46,9 @@ final readonly class CreateUsersTableMigration implements MigrationInterface
     {
         yield <<<'SQL'
             CREATE TABLE IF NOT EXISTS `users` (
-              `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-              `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+                `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
             SQL;
     }
